488b72555d3532974cd22c565e51b40c91c83d23
sean rice <contact@sean.ee>
2022-07-11 01:07:26 -0400

    * add health_check integration test
    * refactor `zero2prod::run` to accept a `Vec<TcpListener>` which are
    used for the app server.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .bind("::1:8000")?
-        .run()
-        .await
+pub fn run(listeners: Vec<TcpListener>) -> Result<Server, std::io::Error> {
+    let server = {
+        let mut server =
+            HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)));
+        for listener in listeners {
+            server = server.listen(listener)?;
+        }
+        server.run()
+    };
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    let addresses: Vec<TcpListener> = ["127.0.0.1:8000", "::1:8000"]
+        .map(|address| TcpListener::bind(address).unwrap())
+        .into();
+    run(addresses)?.await
 }
